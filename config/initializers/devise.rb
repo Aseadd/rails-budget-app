ea164c35c8ca7b76852ff86af37f431d33ccc0f5
addis <betsega23@gmail.com>
2022-12-22 20:41:00 +0300

    Remove Devise.setup configb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-require 'devise'
+
 # Assuming you have not yet modified this file, each configuration option below
 # is set to its default value. Note that some are commented out while others
 # are not: uncommented lines are intended to protect your configuration from
@@ -8,7 +8,7 @@ require 'devise'
 #
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
-Devise.setup do |config|
+# Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
